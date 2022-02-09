bbb6fd337dc49cac5681628ca3498444ba17b205
Jake Walker <jacobm.walker@gmail.com>
2022-02-08 21:37:33 -0500

    Group Post Added

---
+++ GraysSportAlmanac.Model/GroupPostListItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraysSportAlmanac.Model
+{
+    public class GroupPostListItem
+    {
+        [Key]
+        public Guid GroupPostId { get; set; }
+
+        /*
+            [ForeignKey(nameof(Group))]
+            public Group GroupId { get; set; }
+
+            [ForeignKey(nameof(Profile))]
+            public Profile ProfileId { get; set; }
+        */
+
+        public DateTime BetDate { get; set; }
+        public decimal Risked { get; set; }
+        public int Odds { get; set; }
+        public string Result { get; set; }
+        public decimal Payout { get; set; }
+    }
+}
